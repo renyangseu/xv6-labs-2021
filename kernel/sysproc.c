a91f36908379dba3a8a44532ef61a4eeb46a6d09
renyangseu <renyangseu@gmail.com>
2025-05-15 10:14:29 +0800

    lab4:Alarm

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -96,3 +96,23 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+
+uint64
+sys_sigalarm(void)
+{
+  struct proc *p = myproc();
+  
+  if(argint(0, &p->alarm_interval) < 0 && argaddr(1, &p->alarm_handler) < 0)
+    return -1;
+  return 0;
+}
+
+uint64
+sys_sigreturn(void)
+{
+  struct proc *p = myproc();
+  memmove(p->trapframe, p->alarm_trapframe, sizeof(struct trapframe));
+  p->is_alarming = 0;
+  return 0;
+}

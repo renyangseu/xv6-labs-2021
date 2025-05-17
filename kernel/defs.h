2f2a0fe94d9a3955252ae2b0422ecfa561d9188e
renyangseu <renyangseu@gmail.com>
2025-05-17 15:21:02 +0800

    Lab6: Copy-on-Write Fork

--- kernel/defs.h
+++ kernel/defs.h
@@ -63,7 +63,9 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
-
+void            incref(uint64 pa);
+int             decref(void *pa);
+int             getref(void *pa);
 // log.c
 void            initlog(int, struct superblock*);
 void            log_write(struct buf*);
@@ -145,6 +147,7 @@ void            trapinit(void);
 void            trapinithart(void);
 extern struct spinlock tickslock;
 void            usertrapret(void);
+int             cowfault(pagetable_t pagetable, uint64 va);
 
 // uart.c
 void            uartinit(void);
@@ -170,6 +173,7 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+pte_t *         walk(pagetable_t pagetable, uint64 va, int alloc);
 
 // plic.c
 void            plicinit(void);
--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -13,7 +13,8 @@ void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
-
+#define NPAGE (PHYSTOP/PGSIZE)
+int refcount[NPAGE];
 struct run {
   struct run *next;
 };
@@ -35,8 +36,10 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+    refcount[(uint64)p / PGSIZE] = 1;
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -50,6 +53,17 @@ kfree(void *pa)
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
+
+  acquire(&kmem.lock);
+  int pn = (uint64)pa / PGSIZE;
+  if (refcount[pn] < 1)
+    panic("kfree ref");
+  refcount[pn] -= 1;
+  int tmp = refcount[pn];
+  release(&kmem.lock);
+
+  if (tmp > 0)
+    return;
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
@@ -72,11 +86,48 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if (r)
+  {
+    int pn = ((uint64)r) / PGSIZE;
+    if (refcount[pn] != 0)
+      panic("kalloc ref");
+    refcount[pn] = 1;
     kmem.freelist = r->next;
+  }
   release(&kmem.lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
+}
+
+void incref(uint64 pa)
+{
+  int pn = ((uint64)pa) / PGSIZE;
+  acquire(&kmem.lock);
+  if (pa > PHYSTOP || refcount[pn] < 1)
+    panic("incref failed");
+  refcount[pn] += 1;
+  release(&kmem.lock);
+}
+
+int decref(void *pa)
+{
+  acquire(&kmem.lock);
+  int pn = ((uint64)pa) / PGSIZE;
+  if(refcount[pn] < 1)
+    panic("kfree ref failed");
+  --refcount[pn];
+  int cnt = refcount[pn];
+  release(&kmem.lock);
+  return cnt;
+}
+
+int getref(void *pa)
+{
+  acquire(&kmem.lock);
+  int pn = (uint64)pa / PGSIZE;
+  int tmp = refcount[pn];
+  release(&kmem.lock);
+  return tmp;
 }

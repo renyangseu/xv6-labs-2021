77cf30f152411c2bd7682ca0ca90986a4afb2b88
renyangseu <renyangseu@gmail.com>
2025-06-16 18:54:00 +0800

    lab:Symbolic links

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -330,6 +330,34 @@ sys_open(void)
     return -1;
   }
 
+  if(ip->type == T_SYMLINK && !(omode & O_NOFOLLOW)){
+    // 处理符号链接
+    for(int i = 0; i < 10; i++){  // 防止循环链接
+      if(readi(ip, 0, (uint64)path, 0, MAXPATH) <= 0){
+        iunlockput(ip);
+        end_op();
+        return -1;
+      }
+
+      iunlockput(ip);
+      ip = namei(path);
+      if(ip == 0){
+        end_op();
+        return -1;
+      }
+
+      ilock(ip);
+      if(ip->type != T_SYMLINK)
+        break;
+    }
+
+    if(ip->type == T_SYMLINK){
+      iunlockput(ip);
+      end_op();
+      return -1;
+    }
+  }
+
   if(ip->type == T_DEVICE){
     f->type = FD_DEVICE;
     f->major = ip->major;
@@ -482,5 +510,34 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+// Create the path new as a symlink
+uint64
+sys_symlink(void)
+{
+  char target[MAXPATH], path[MAXPATH];
+  struct inode *ip;
+
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+  // 创建新inode，类型为T_SYMLINK
+  if((ip = create(path, T_SYMLINK, 0 , 0)) == 0){
+    end_op();
+    return -1;
+  }
+
+  if(writei(ip, 0, (uint64)target, 0, strlen(target)) != strlen(target))
+  {
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+
+  iunlockput(ip);
+  end_op();
   return 0;
 }
